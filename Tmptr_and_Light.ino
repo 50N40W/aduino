3da744e982f064fc2f024fa90f3bdd4013a3e9ce
H <50N40W@users.noreply.github.com>
2020-05-20 19:23:17 -0400

    Update with SD logging
    Add support for SD card data logging.

--- Tmptr_and_Light.ino
+++ Tmptr_and_Light.ino
@@ -1,12 +1,15 @@
 #include <Wire.h>
 #include <Adafruit_BMP085.h>
 #include <SimpleDHT.h>
+#include <SPI.h>
+#include <SD.h>
 
+const int chipSelect = 4;
 const int photoRPin = 0;
 const int ledPin = LED_BUILTIN;
 const int pinDHT11 = 2;
-const long PHOTO_PERIOD = 20;
-const long PERIOD = 500;
+const long PHOTO_PERIOD = 200;
+const long PERIOD = 1000;
 const long DHT_PERIOD = 1500;
 const float LIGHT_FC = 0.94;
 
@@ -34,15 +37,24 @@ void setup(void)
     Serial.print("BMP 180 not detected");
     while(1);
   }
+  Serial.print("Initializing SD card...");
+
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    // don't do anything more:
+    //while (1);
+  }
 }
 
 void loop(void) 
 {
-
   unsigned long currentMillis = millis();
   if (currentMillis - prevDhtMillis > DHT_PERIOD) {
     prevDhtMillis = currentMillis;
     int err = SimpleDHTErrSuccess;
+    ledState = (ledState == LOW)? HIGH : LOW;
+    digitalWrite(ledPin, ledState);
     if ((err = dht11.read(&dht_tmptr, &dht_humidity, NULL)) != SimpleDHTErrSuccess) {
       Serial.print("Read DHT11 failed, err="); 
       Serial.println(err);
@@ -55,17 +67,20 @@ void loop(void)
   }
   if (currentMillis - previousMillis >= PERIOD) {
     previousMillis = currentMillis;
-    digitalWrite(ledPin, ledState);
-    ledState = (ledState == LOW)? HIGH : LOW;
+    String dataString = String(currentMillis) + ", ";
+    dataString += String(dht_humidity) + ", " + String(dht_tmptr) + ", ";
+    dataString += String(bmp.readTemperature()) + ", " + String(bmp.readPressure()) + ", ";
+    dataString += String(lightLevel);
 
-    Serial.print(currentMillis);
-    Serial.print(", ");
-    Serial.print(bmp.readTemperature());
-    Serial.print(", ");
-    Serial.print(dht_tmptr);
-    Serial.print(", ");
-    Serial.print(lightLevel);    
-    Serial.print(", ");
-    Serial.println(bmp.readPressure());
+    Serial.print(dataString);
+
+    File dataFile = SD.open("datalog.txt", FILE_WRITE);
+    if (dataFile) {
+      dataFile.println(dataString);
+      dataFile.close();
+      Serial.println("");
+    }
+    else {
+      Serial.println(", unable to open file on SD card  ");
+    }
   }
-}
