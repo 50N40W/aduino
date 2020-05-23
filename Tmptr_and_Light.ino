e677a0e87befa5d73543a40594970e3538021da9
H <50N40W@users.noreply.github.com>
2020-05-22 20:45:25 -0400

    Add second photocell,
    Also changed filter constant, messed with the order of execution and some other small changes related to how the data is reported.

--- Tmptr_and_Light.ino
+++ Tmptr_and_Light.ino
@@ -6,18 +6,22 @@
 
 const int chipSelect = 4;
 const int photoRPin = 0;
+const int photoRPin2 = 1;
 const int ledPin = LED_BUILTIN;
 const int pinDHT11 = 2;
 const long PHOTO_PERIOD = 200;
-const long PERIOD = 1000;
+const long PERIOD = 5000;
 const long DHT_PERIOD = 1500;
-const float LIGHT_FC = 0.94;
+const float LIGHT_FC = 0.7;
 
 int ledState = LOW;
 unsigned long previousMillis = 0;
 unsigned long prevDhtMillis = 0;
 unsigned long photoMillis = 0;
+unsigned long lampMillis = 0;
+unsigned long lamp_period = 1000;
 int lightLevel = 500;
+int light2 = 0;
 byte dht_tmptr = 0;
 byte dht_humidity = 0;
 
@@ -37,7 +41,7 @@ void setup(void)
     Serial.print("BMP 180 not detected");
     while(1);
   }
-  Serial.print("Initializing SD card...");
+  Serial.println("Initializing SD card...");
 
   // see if the card is present and can be initialized:
   if (!SD.begin(chipSelect)) {
@@ -50,27 +54,41 @@ void setup(void)
 void loop(void) 
 {
   unsigned long currentMillis = millis();
+
   if (currentMillis - prevDhtMillis > DHT_PERIOD) {
     prevDhtMillis = currentMillis;
     int err = SimpleDHTErrSuccess;
-    ledState = (ledState == LOW)? HIGH : LOW;
-    digitalWrite(ledPin, ledState);
     if ((err = dht11.read(&dht_tmptr, &dht_humidity, NULL)) != SimpleDHTErrSuccess) {
       Serial.print("Read DHT11 failed, err="); 
       Serial.println(err);
+      lamp_period = 500;
     }
+    else {
+      lamp_period = PERIOD;
+    }
+  }
+    if (currentMillis - lampMillis > lamp_period) {
+    lampMillis = currentMillis;
+    if (ledState == LOW) {
+      ledState = HIGH;
+    } else {
+      ledState = LOW;
+    }
+    digitalWrite(ledPin, ledState);
   }
   if (currentMillis - photoMillis >= PHOTO_PERIOD) {
     photoMillis = currentMillis;
     int rawLightLevel = analogRead(photoRPin);
     lightLevel = (int)(LIGHT_FC * (float)lightLevel + (1-LIGHT_FC)*(rawLightLevel));
+    int rawLight2 = analogRead(photoRPin2);
+    light2 = (int)(LIGHT_FC * (float)light2 + (1-LIGHT_FC)*(rawLight2));
   }
   if (currentMillis - previousMillis >= PERIOD) {
     previousMillis = currentMillis;
     String dataString = String(currentMillis) + ", ";
     dataString += String(dht_humidity) + ", " + String(dht_tmptr) + ", ";
     dataString += String(bmp.readTemperature()) + ", " + String(bmp.readPressure()) + ", ";
-    dataString += String(lightLevel);
+    dataString += String(lightLevel) + ", " + String(light2);
 
     Serial.print(dataString);
 
@@ -84,3 +102,4 @@ void loop(void)
       Serial.println(", unable to open file on SD card  ");
     }
   }
+}
